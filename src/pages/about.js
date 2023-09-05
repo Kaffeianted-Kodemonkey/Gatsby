67549301f6998c531f1b584f8da86cb235886038
kfn8tedkodemonkey <kaffeinatedkodemonkey@gmail.com>
2023-09-05 00:13:40 -0600

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,19 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
-      <p>I am going to be a pro at Gatsby.</p>
+    <Layout pageTitle="About">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Kodemonkey, A Digital A11y Provder"
+        src="../images/KKM-Logo.png"
+      />
     </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <title>About</title>
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle = "Home">
         <p>I'm making this by following the Gatsby Tutorial.</p>
-    </Layout>
+     </Layout>
   )
 }
 
